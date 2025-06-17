6bd3d35d32f200b7ad37ac1a6620a6a6d1c83b1d
vinngo <ngovincent9593@gmail.com>
2025-06-17 00:30:45 -0700

    fix: profile CRUD

--- src/lib/stores/rootStore.tsx
+++ src/lib/stores/rootStore.tsx
@@ -11,6 +11,7 @@ interface RootStoreState {
   error: string | null;
   initialized: boolean;
   fetchCoreData: () => Promise<void>;
+  fetchCoreForce: () => Promise<void>;
 }
 
 export const useRootStore = create<RootStoreState>((set) => ({
@@ -30,6 +31,46 @@ export const useRootStore = create<RootStoreState>((set) => ({
       return;
     }
 
+    try {
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+      if (!user || userError) {
+        throw new Error("User not found");
+      }
+      const { data: appUser, error: appUserError } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", user.id)
+        .single();
+      if (!appUser || appUserError) {
+        throw new Error("Failed to fetch user data");
+      }
+      console.log("got app user: ", appUser);
+      const [houseRes, memberRes] = await Promise.all([
+        supabase.from("houses").select("*").eq("id", appUser.house_id).single(),
+        supabase
+          .from("house_members")
+          .select("*")
+          .eq("house_id", appUser.house_id),
+      ]);
+      set({
+        user: appUser,
+        email: appUser.email,
+        house: houseRes.data,
+        houseMembers: memberRes.data,
+        loading: false,
+        error: null,
+        initialized: true,
+      });
+    } catch (e) {
+      set({ error: String(e), loading: false });
+    }
+  },
+
+  async fetchCoreForce() {
+    set({ loading: true, error: null });
     try {
       const {
         data: { user },
